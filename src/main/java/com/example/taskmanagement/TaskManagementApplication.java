66ee04f9ba2b8fa7b38a3773610ba625d647ed30
Satyanarayana Bandi <satishbandi2003@gmail.com>
2026-09-24 12:42:42 +0530

    test Jenkins CI CD code change

--- src/main/java/com/example/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/example/taskmanagement/TaskManagementApplication.java
@@ -1,4 +1,3 @@
-
 package com.example.taskmanagement;
 
 import org.springframework.boot.SpringApplication;
@@ -9,8 +8,9 @@ public class TaskManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagementApplication.class, args);
+
+        System.out.println("NEW CODE DEPLOYED THROUGH JENKINS");
     }
 
     // Jenkins webhook test
 }
-
